afe7cdba5c2a6158c09d9d094cc25f44ff533fa4
AbhishekNayak01 <abhishek.nayak@saarthi.ai>
2023-10-08 17:50:01 +0530

    added style for card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 import style from "./Card.module.scss"
 
 interface CardProps {
-    img : string;
-    title : string;
-    type : string
-    year : string
+    img: string;
+    title: string;
+    type: string
+    year: string
 }
-function Card(props :CardProps) {
-  return (
-    <div className={style.cardWrapper}>
-        <img className={style.imgPoster} src={props.img}></img>
-        <div className={style.title}>{props.title}</div>
-        <div className={style.infoDiv}>
-          <span className={style.detail}>{props.year}</span>
-          <span className={style.detail}>{props.type}</span>
+function Card(props: CardProps) {
+    return (
+        <div className={style.cardWrapper}>
+            <img className={style.imgPoster} src={props.img}></img>
+            <div className={style.title}>{props.title}</div>
+            <div className={style.infoDiv}>
+                <span className={style.styleDetails}>{props.year}</span>
+                <span className={style.styleDetails}>{props.type}</span>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
